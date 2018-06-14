9a26ddd8ccf547539746c8bbb0d0c30a039ce205
Jason Pitts <juniorjr083@gmail.com>
2018-06-14 18:26:15 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -16,6 +16,7 @@ def game_hash
                     blocks: 1,
                     slam_dunks: 1
                 },
+                {
                 :player_name => "Reggie Evans",
                     number: 30,
                     shoe: 14,
@@ -25,7 +26,9 @@ def game_hash
                     steals: 12,
                     blocks: 12,
                     slam_dunks: 7
-                },
+                },{
+                  
+                }
                 :player_name => "Brook Lopez",
                     number: 11,
                     shoe: 17,
@@ -35,8 +38,10 @@ def game_hash
                     steals: 3,
                     blocks: 1,
                     slam_dunks: 15
-                },
-                "Mason Plumlee"
+                },{
+                  
+                }
+                :player_name => "Mason Plumlee",
                     number: 1,
                     shoe: 19,
                     points: 26,
@@ -46,7 +51,8 @@ def game_hash
                     blocks: 8,
                     slam_dunks: 5
                 },
-                "Jason Terry" => {
+                {
+                :player_name => "Jason Terry",
                     number: 31,
                     shoe: 15,
                     points: 19,
